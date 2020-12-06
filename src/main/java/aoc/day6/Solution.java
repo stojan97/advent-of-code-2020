e6b00d66f7b4e634279e41a95f8e7aa1e52419b3
stojan97 <stoki97.basketball@hotmail.com>
2020-12-06 19:55:06 +0100

    day6 Solution
    day6 Solution

---
+++ src/main/java/aoc/day6/Solution.java
@@ -0,0 +1,65 @@
+package aoc.day6;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import aoc.utils.FileReader;
+
+public class Solution {
+
+  public static void main(String[] args) {
+
+    List<List<String>> lines = FileReader.readLinesByGroups("aoc/day6/input.txt");
+
+    int count = getCounts(lines);
+    int countsAnsweredByEveryone = getCountsAnsweredByEveryone(lines);
+    System.out.println(count);
+    System.out.println(countsAnsweredByEveryone);
+  }
+
+  private static int getCounts(List<List<String>> lines) {
+
+    int count = 0;
+
+    for (List<String> group: lines) {
+      Set<Character> seenFields = new HashSet<>();
+
+      for (String line : group) {
+        for (char c : line.toCharArray()) {
+          seenFields.add(c);
+        }
+      }
+      count += seenFields.size();
+    }
+
+    return count;
+  }
+
+  private static int getCountsAnsweredByEveryone(List<List<String>> lines) {
+
+    int count = 0;
+
+    for (List<String> group: lines) {
+      Map<Character, Integer> seenFields = new HashMap<>();
+
+      for (String line : group) {
+        for (char c : line.toCharArray()) {
+          seenFields.put(c, seenFields.getOrDefault(c, 0) + 1);
+        }
+      }
+
+      for (Map.Entry<Character, Integer> entry : seenFields.entrySet()) {
+        if (entry.getValue() == group.size()) {
+         count++;
+        }
+      }
+
+    }
+
+    return count;
+  }
+
+}
