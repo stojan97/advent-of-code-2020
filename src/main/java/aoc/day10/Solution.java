4ef0eacd50c456f2a093e98ec9d0d8cc77ca71a2
stojan97 <stoki97.basketball@hotmail.com>
2020-12-10 13:14:10 +0100

    day10 solution

--- src/main/java/aoc/day10/Solution.java
+++ src/main/java/aoc/day10/Solution.java
@@ -45,7 +45,7 @@ public class Solution {
 
   private static long getNumberOfArrangements(List<Integer> adapters) {
 
-    Map<Integer, Long> dpMap = adapters.stream().collect(Collectors.toMap(Integer::intValue, integer -> 0L));
+    Map<Integer, Long> dpMap = adapters.stream().collect(Collectors.toMap(Integer::intValue, arrangements -> 0L));
     dpMap.put(0, 1L);
 
     for (int i = 1; i < adapters.size(); i++) {
